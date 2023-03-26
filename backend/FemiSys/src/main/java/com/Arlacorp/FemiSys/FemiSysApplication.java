de31a361326b95c27701baef9d88f6fa86666b7b
Pancratzia <ortegalaura180999@gmail.com>
2023-03-26 19:01:34 -0400

    Se agrega el inicializer del backend en Spring

---
+++ backend/FemiSys/src/main/java/com/Arlacorp/FemiSys/FemiSysApplication.java
@@ -0,0 +1,13 @@
+package com.Arlacorp.FemiSys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FemiSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FemiSysApplication.class, args);
+	}
+
+}
---
+++ backend/FemiSys/src/test/java/com/Arlacorp/FemiSys/FemiSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Arlacorp.FemiSys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FemiSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
